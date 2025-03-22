99f2852d5a3b355a524962656690f8e9c59ed5aa
gabrielhdzdaw <gabrielhernandezdaw@gmail.com>
2025-03-22 17:21:19 +0100

    Añadidas las clases Personaje, Jugador, Enemigo, Item y Combate

---
+++ Combate.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectoRPG
+{
+    internal class Combate
+    {
+        Jugador jugador;
+        Enemigo enemigo;
+
+        public Combate(Jugador jugador, Enemigo enemigo)
+        {
+            this.jugador = jugador;
+            this.enemigo = enemigo;
+        }
+    }
+}
---
+++ Item.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectoRPG
+{
+    internal abstract class Item // Habrá subclases como Arma, Armadura, Pocion, etc.
+    {
+        string nombre;
+        string descripcion;
+        int precio;
+
+        public Item(string nombre, string descripcion, int precio)
+        {
+            this.nombre = nombre;
+            this.descripcion = descripcion;
+            this.precio = precio;
+        }
+
+        public string GetNombre()
+        {
+            return nombre;
+        }
+
+        public string GetDescripcion()
+        {
+            return descripcion;
+        }
+
+        public int GetPrecio()
+        {
+            return precio;
+        }
+
+        public void SetNombre(string nombre)
+        {
+            this.nombre = nombre;
+        }
+
+        public void SetDescripcion(string descripcion)
+        {
+            this.descripcion = descripcion;
+        }
+
+        public void SetPrecio(int precio)
+        {
+            this.precio = precio;
+        }
+
+        public override string ToString()
+        {
+            return "Item: " + nombre + " Descripcion: " + descripcion + " Precio: " + precio;
+        }
+
+
+    }
+}
