e325c536c4fffa71a800fe7aae095932c5c9757f
alvmarcer <alvmarcer@alu.edu.gva.es>
2025-04-10 23:32:07 +0200

    Implementación de las ventanas de arranque

--- Dibujar.cs
+++ Dibujar.cs
@@ -19,7 +19,28 @@ namespace ProyectoRPG
             }
             return anchuraMaxima;
         }
-        public static void DibujarSprite(int x, int y, string dibujo)
+
+        public static void DibujarSpriteNormal(int x, int y, string dibujo)
+        {
+            int anchuraDibujo = GetAnchuraDibujo(dibujo);
+
+            string[] lineas = dibujo.Split('\n');
+            for (int i = 0; i < lineas.Length; i++)
+            {
+                Console.SetCursorPosition(x, y + i);
+
+                if (i == lineas.Length - 1)
+                {
+                    Console.Write(lineas[i] + "\n");
+                }
+                else
+                {
+                    Console.Write(lineas[i]);
+                }
+            }
+        }
+
+        public static void DibujarSpriteCentrado(int x, int y, string dibujo)
         {
             int anchuraDibujo = GetAnchuraDibujo(dibujo);
             int comienzoX = x - (anchuraDibujo / 2);
@@ -40,18 +61,18 @@ namespace ProyectoRPG
             }
         }
 
-        public static void DibujarRectangulo(int x, int y, int anchura, int altura, char caracter)
+        public static void DibujarRectangulo(int x, int y, char caracter)
         {
-            for (int i = 0; i <= altura; i++)
+            for (int i = 0; i <= Program.ALTURA_RECTANGULO; i++)
             {
-                for (int j = 0; j <= anchura; j++)
+                for (int j = 0; j <= Program.ANCHURA_RECTANGULO; j++)
                 {
                     Console.SetCursorPosition(x + j, y + i);
-                    if (i == 0 || i == altura || j == 0 || j == anchura)
+                    if (i == 0 || i == Program.ALTURA_RECTANGULO || j == 0 || j == Program.ANCHURA_RECTANGULO)
                     {
-                        if (j == anchura || i == altura)
+                        if (j == Program.ANCHURA_RECTANGULO || i == Program.ALTURA_RECTANGULO)
                         {
-                            Console.Write(caracter + "\n");
+                            Console.Write(caracter);
                         }
                         else
                         {
@@ -60,6 +81,125 @@ namespace ProyectoRPG
                     }
                 }
             }
+        }
+
+        private static void LimpiarPantallaSimple(int x, int y, int maxAncho, int maxAlto)
+        {
+            for (int i = x; i < x + maxAncho; i++)
+            {
+                for (int j = y; j < maxAlto; j++)
+                {
+                    Console.SetCursorPosition(i, j);
+                    Console.Write(" ");
+                }
+            }
+        }
+
+        public static void LimpiarPantalla(int x, int y, int maxAncho, int maxAlto)
+        {
+            Console.BackgroundColor = ConsoleColor.Gray;
+            LimpiarPantallaSimple(x, y, maxAncho, maxAlto);
+            Console.ResetColor();
+            LimpiarPantallaSimple(x, y, maxAncho, maxAlto);
+        }
+
+        public static int ContarSaltos(string cadena)
+        {
+            int resultado = 0;
+
+            if (cadena.Contains('\n'))
+            {
+                foreach (char c in cadena)
+                {
+                    if(c == '\n')
+                    {
+                        resultado++;
+                    }
+                }
+            }
+
+            return resultado;
+        }
+
+        public static void EscribirTexto(string cadena)
+        {
+            foreach (char c in cadena)
+            {
+                if (c == '.')
+                {
+                    Thread.Sleep(250);
+                }
+
+                Console.Write(c);
+
+                if (c == '.')
+                {
+                    Thread.Sleep(250);
+                }
+                else
+                {
+                    Thread.Sleep(30);
+                }
+            }
+        }
+
+        public static void Inicio(int inicioX, int inicioY)
+        {
+            int x = inicioX + (Program.ANCHURA_RECTANGULO / 2);
+            int y = inicioY + (Program.ALTURA_RECTANGULO / 2);
+
+            Console.CursorVisible = false;
+
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Dibujar.DibujarSpriteCentrado(x, y - 3, "¡ ADVERTENCIA !");
+            Console.ResetColor();
+
+            Dibujar.DibujarSpriteCentrado(x, y - 1, "Se recomienda poner la pantalla completa (F11) para mejor expriencia");
+            Dibujar.DibujarSpriteCentrado(x, y + 1, "Para continuar pulse ENTER...");
+
+            bool pantallaCompleta = false;
+            while (!pantallaCompleta)
+            {
+                if (Console.KeyAvailable)
+                {
+                    pantallaCompleta = Console.ReadKey().Key == ConsoleKey.Enter;
+                }
+            }
+            Console.CursorVisible = true;
+
+            Dibujar.LimpiarPantalla(inicioX, inicioY, Program.ANCHURA_RECTANGULO - 1, Program.ALTURA_RECTANGULO + inicioY - 1);
+
+            string titulo = "\r\n██████╗░██████╗░░██████╗░" +
+                            "\r\n██╔══██╗██╔══██╗██╔════╝░" +
+                            "\r\n██████╔╝██████╔╝██║░░██╗░" +
+                            "\r\n██╔══██╗██╔═══╝░██║░░╚██╗" +
+                            "\r\n██║░░██║██║░░░░░╚██████╔╝" +
+                            "\r\n╚═╝░░╚═╝╚═╝░░░░░░╚═════╝░";
+            Dibujar.DibujarSpriteCentrado(x, y - Dibujar.ContarSaltos(titulo) - 2, titulo);
+
+            string subtitulo1 = "En busca del demonio malo malísimo, MUY MUY malo";
+            Console.SetCursorPosition(x - (subtitulo1.Length / 2), y);
+            Dibujar.EscribirTexto(subtitulo1);
+            Dibujar.EscribirTexto("...");
+
+            string subtitulo2 = "Pulse ENTER para comenzar la aventura";
+            Console.SetCursorPosition(x - (subtitulo2.Length / 2), y + 2);
+            Dibujar.EscribirTexto(subtitulo2);
+
+            Console.CursorVisible = false;
+
+            bool enterPulsado = false;
+
+            while (!enterPulsado)
+            {
+                if (Console.KeyAvailable)
+                {
+                    enterPulsado = Console.ReadKey().Key == ConsoleKey.Enter;
+                }
+            }
+
+            Dibujar.LimpiarPantalla(inicioX, inicioY, Program.ANCHURA_RECTANGULO - 1, Program.ALTURA_RECTANGULO + inicioY - 1);
+            Console.CursorVisible = true;
         }
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,6 +1,14 @@
-﻿/* Autores: 
-/* 
-*/
+﻿/* AUTORES:
+ * 
+ * [ IMPORTANTE ] Poner los apellidos
+ * 
+ * Gabriel
+ * Lucía
+ * Álvaro
+ * Dayron
+ * Carlos
+ */
+
 using System;
 using System.Runtime.InteropServices;
 
@@ -9,6 +17,10 @@ namespace ProyectoRPG
 {
     internal class Program
     {
+        public const int ANCHURA_RECTANGULO = 176; // Dentro el ancho es de 175
+        public const int ALTURA_RECTANGULO = 44; // Dentro el ancho es de 43
+
+        // Ajustes para la pantalla
         [DllImport("kernel32.dll", SetLastError = true)]
         static extern IntPtr GetConsoleWindow();
 
@@ -16,25 +28,92 @@ namespace ProyectoRPG
         static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
 
         const int SW_MAXIMIZE = 3;
-        public static void Main(string[] args)
-        {
 
-            //Maximizar la ventana al incio
+        public static void PrepararVentanaInicio()
+        {
+            Console.Title = "RPG";
             Console.SetBufferSize(Console.LargestWindowWidth, Console.LargestWindowHeight);
             Console.SetWindowSize(Console.LargestWindowWidth, Console.LargestWindowHeight);
             IntPtr consoleWindow = GetConsoleWindow();
             ShowWindow(consoleWindow, SW_MAXIMIZE);
+        }
 
-            //Todo el juego se mostrará dentro de 174 - 70
+        public static int Menu(int x, int y) // [ IMPORTANTE ] Versión gráfica y lógica del menú, tocará cambiarla de sitio y demás
+        {
+            Console.CursorVisible = false;
 
-            int anchuraRectangulo = 175;
-            int alturaRectangulo = 71;
+            int centroX = x + (ANCHURA_RECTANGULO / 2) - 1;
+            int centroY = y + (ALTURA_RECTANGULO / 2) - 1;
 
-            int x = (Console.WindowWidth - anchuraRectangulo) / 2;
-            int y = (Console.WindowHeight - alturaRectangulo) / 2;
+            Dibujar.DibujarSpriteCentrado(centroX, centroY - 4, "\r\n█▀█ █▀█ █▀▀" +
+                                                                "\r\n█▀▄ █▀▀ █▄█");
 
-            Dibujar.DibujarRectangulo(x, y, anchuraRectangulo, alturaRectangulo, '▓');
+            string[] opciones = ["Continuar","Nueva partida","Records","Créditos","Salir"];
+            int opcion = 0;
 
+            // Repetir este for hasta que se pulse enter
+            ConsoleKeyInfo tecla = new ConsoleKeyInfo();
+
+            while (tecla.Key != ConsoleKey.Enter)
+            {
+                int espaciadoVertical = 0;
+                for (int i = 0; i < opciones.Length; i++)
+                {
+                    if (opcion == i)
+                    {
+                        Console.BackgroundColor = ConsoleColor.White;
+                        Console.ForegroundColor = ConsoleColor.Black;
+                    }
+
+                    Dibujar.DibujarSpriteCentrado(centroX - 1, centroY - espaciadoVertical, opciones[i]);
+                    espaciadoVertical -= 1;
+
+                    if (opcion == i)
+                    {
+                        Console.ResetColor();
+                    }
+                }
+
+                if(Console.KeyAvailable)
+                {
+                    tecla = Console.ReadKey();
+
+                    switch(tecla.Key)
+                    {
+                        case ConsoleKey.UpArrow:
+                            if (opcion - 1 >= 0)
+                                opcion--;
+                            break;
+                        case ConsoleKey.DownArrow:
+                            if (opcion + 1 < opciones.Length)
+                                opcion++;
+                            break;
+                    }
+                }
+            }
+
+            Console.CursorVisible = true;
+
+            return opcion;
+        }
+
+        public static void Main(string[] args)
+        {
+            PrepararVentanaInicio();
+
+            int x = (Console.WindowWidth - ANCHURA_RECTANGULO) / 2 + 1;
+            int y = (Console.WindowHeight - ALTURA_RECTANGULO) / 2 + 1;
+
+            Dibujar.DibujarRectangulo(x - 1, y - 1, '▓');
+
+            Dibujar.Inicio(x, y);
+
+            int opcion = 0;
+            while (opcion != 4)
+            {
+                opcion = Menu(x, y);
+                // Arranca todo aquí
+            }
         }
     }
 }
