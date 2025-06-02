9cab5c341a2f43d78537cd1fc40dccec8f85f5e1
Dyron2704 <dayluccor@alu.edu.gva.es>
2025-06-02 20:17:18 +0200

    Añadidos minijuegos Magos y Dados

---
+++ Minijuegos/MinijuegoDados.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectoRPG.Minijuegos
+{
+    internal class MinijuegoDados : Minijuego
+    {
+        public int TiempoAnimacion;
+        public int PasosAnimacion;
+        public Random Random;
+
+        public MinijuegoDados()
+        {
+            TiempoAnimacion = 2000;
+            PasosAnimacion = 10;
+            Random = new Random();
+        }
+
+        public MinijuegoDados(int tiempoAnimacion, int pasosAnimacion)
+        {
+            TiempoAnimacion = tiempoAnimacion;
+            PasosAnimacion = pasosAnimacion;
+            Random = new Random();
+        }
+
+        public void Jugar()
+        {
+            int dadoJugador, dadoMaquina;
+
+            Console.Clear();
+            Console.WriteLine("¡Comienza la batalla!");
+            Console.WriteLine("Presiona una tecla para tirar los dados...");
+
+            do
+            {
+                Console.ReadKey(true);
+                Console.Write("\nTirando los dados");
+                for (int i = 0; i < 3; i++)
+                {
+                    Thread.Sleep(1000);
+                    Console.Write(" .");
+                }
+
+                Console.WriteLine();
+
+                int delay = TiempoAnimacion / PasosAnimacion;
+                int mostrarJugador = 0, mostrarMaquina = 0;
+
+                for (int i = 0; i < PasosAnimacion; i++)
+                {
+                    mostrarJugador = Random.Next(1, 7);
+                    mostrarMaquina = Random.Next(1, 7);
+
+                    Console.Clear();
+                    Console.WriteLine("¡Comienza la batalla!");
+                    Console.WriteLine("\nDados girando...");
+
+                    MostrarDadosCentrados(mostrarJugador, mostrarMaquina);
+
+                    Thread.Sleep(delay);
+                }
+
+                dadoJugador = mostrarJugador;
+                dadoMaquina = mostrarMaquina;
+
+                Console.WriteLine($"\nTú sacaste: {dadoJugador}");
+                Console.WriteLine($"El enemigo sacó: {dadoMaquina}");
+
+                if (dadoJugador == dadoMaquina)
+                {
+                    Console.WriteLine("Empate, ¡se repite la tirada!");
+                    Thread.Sleep(2000);
+                    Console.Clear();
+                }
+
+            } while (dadoJugador == dadoMaquina);
+
+            if (dadoJugador > dadoMaquina)
+                Console.WriteLine("Has derrotado al enemigo");
+            else
+                Console.WriteLine("El enemigo ganó la batalla");
+        }
+
+        public void MostrarDadosCentrados(int valorJugador, int valorMaquina)
+        {
+            string[][] dados =
+            {
+                new string[]{"+-------+","|       |","|   *   |","|       |","+-------+"},
+                new string[]{"+-------+","| *     |","|       |","|     * |","+-------+"},
+                new string[]{"+-------+","| *     |","|   *   |","|     * |","+-------+"},
+                new string[]{"+-------+","| *   * |","|       |","| *   * |","+-------+"},
+                new string[]{"+-------+","| *   * |","|   *   |","| *   * |","+-------+"},
+                new string[]{"+-------+","| *   * |","| *   * |","| *   * |","+-------+"}
+            };
+
+            string[] jugador = dados[valorJugador - 1];
+            string[] maquina = dados[valorMaquina - 1];
+
+            int ancho = Console.WindowWidth;
+            int alto = Console.WindowHeight;
+
+            int filaInicio = (alto / 2) - 3;
+            int colInicio = (ancho / 2) - 16;
+
+            for (int i = 0; i < jugador.Length; i++)
+            {
+                Console.SetCursorPosition(colInicio, filaInicio + i);
+                Console.Write(jugador[i] + "     " + maquina[i]);
+            }
+
+            Console.SetCursorPosition(0, filaInicio + 6);
+        }
+    }
+}
---
+++ Minijuegos/MinijuegoPeleaMagos.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectoRPG.Minijuegos
+{
+    internal class MinijuegoPeleaMagos : Minijuego
+    {
+        public const int maxProgreso = 30;
+        public int progresoJugador;
+        public int progresoRival;
+        public DateTime ultimoAvanceRival;
+        public TimeSpan intervaloRival;
+        public ConsoleKey teclaObjetivo;
+        public Random random;
+
+        public MinijuegoPeleaMagos()
+        {
+            progresoJugador = 0;
+            progresoRival = 0;
+            intervaloRival = TimeSpan.FromMilliseconds(660);
+            ultimoAvanceRival = DateTime.Now;
+            random = new Random();
+            teclaObjetivo = GenerarTeclaAleatoria();
+        }
+
+        public void Jugar()
+        {
+            Console.Clear();
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("¡Presiona la tecla que aparece en pantalla!");
+            Console.WriteLine("-------------------------------------------");
+
+            MostrarBarras();
+            MostrarFlecha();
+
+            while (progresoJugador < maxProgreso && progresoRival < maxProgreso)
+            {
+                if (Console.KeyAvailable)
+                {
+                    ConsoleKeyInfo tecla = Console.ReadKey(true);
+                    if (tecla.Key == teclaObjetivo)
+                    {
+                        progresoJugador++;
+                        MostrarBarras();
+                        teclaObjetivo = GenerarTeclaAleatoria();
+                        MostrarFlecha();
+                    }
+                }
+
+                if (DateTime.Now - ultimoAvanceRival >= intervaloRival)
+                {
+                    progresoRival++;
+                    MostrarBarras();
+                    ultimoAvanceRival = DateTime.Now;
+                }
+
+                Thread.Sleep(10);
+            }
+
+            Console.SetCursorPosition(0, 6);
+            if (progresoJugador >= maxProgreso)
+                Console.WriteLine("Has derrotado al enemigo.");
+            else
+                Console.WriteLine("El rival te derrotó.");
+
+            Console.ResetColor();
+        }
+
+        public ConsoleKey GenerarTeclaAleatoria()
+        {
+            ConsoleKey[] teclas = { ConsoleKey.LeftArrow, ConsoleKey.RightArrow };
+            return teclas[random.Next(teclas.Length)];
+        }
+
+        public void MostrarFlecha()
+        {
+            Console.SetCursorPosition(0, 4);
+            Console.Write("Presiona: ");
+            switch (teclaObjetivo)
+            {
+                case ConsoleKey.LeftArrow:
+                    Console.WriteLine("Izquierda");
+                    break;
+                case ConsoleKey.RightArrow:
+                    Console.WriteLine("Derecha  ");
+                    break;
+            }
+        }
+
+        public void MostrarBarras()
+        {
+            Console.SetCursorPosition(0, 2);
+            Console.Write("Jugador: [");
+            Console.Write(new string('█', progresoJugador));
+            Console.Write(new string(' ', maxProgreso - progresoJugador));
+            Console.WriteLine("]");
+
+            Console.Write("Rival:   [");
+            Console.Write(new string('█', progresoRival));
+            Console.Write(new string(' ', maxProgreso - progresoRival));
+            Console.WriteLine("]");
+        }
+    }
+}
